5e71f11d9ba290bbc3288678359e062d8e3fa148
Andy Agus <Redagus@gmail.com>
2020-11-09 11:02:18 -0500

    shopping app

--- webxr-practice/index.js
+++ webxr-practice/index.js
@@ -4,20 +4,43 @@ import {
   StyleSheet,
   Text,
   View,
+  VrButton,
 } from 'react-360';
 
 export default class webxr_practice extends React.Component {
-  render() {
-    return (
-      <View style={styles.panel}>
+  state = {
+    
+  }
+
+  _incrementCount = () => {
+    console.log("hello")
+  }
+
+  render(){
+    return(
+    <View style={styles.panel}>
         <View style={styles.greetingBox}>
           <Text style={styles.greeting}>
-            Welcome to React 360
+            Welcome to the Shopping District
           </Text>
         </View>
-      </View>
-    );
+        <View style={styles.clothingNav}>
+          <Text style={styles.headerText}>
+            What clothes are you looking for?
+          </Text>   
+
+          <VrButton onClick={
+            
+          }>
+            <Text>
+              Shoes
+            </Text>
+          </VrButton>
+        </View>
+    </View>
+    )
   }
+
 };
 
 const styles = StyleSheet.create({
@@ -25,7 +48,7 @@ const styles = StyleSheet.create({
     // Fill the entire surface
     width: 1000,
     height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    backgroundColor: 'rgba(255, 165, 0, .8)',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -38,6 +61,14 @@ const styles = StyleSheet.create({
   greeting: {
     fontSize: 30,
   },
+  clothingNav: {
+    padding: 20,
+    fontSize: 1,
+  },
+  headerText: {
+    backgroundColor: 'blue'
+  },
+
 });
 
 AppRegistry.registerComponent('webxr_practice', () => webxr_practice);
